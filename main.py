95bb60047ed2d14d482b2234893dd4e9591bd869
wwwdo109 <wwwdo109@gmail.com>
2017-11-06 01:17:51 +0900

    mirae_first_commit

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import copy
 
 import converttobinary as ctb
 import bodyposition as bp
-import hand
+import hand as h
 
 # 소스 이미지 가져오기
 # cv::Mat형 자료 -> numppy로 변환 가능 (numpy 배열로 python에서 이미지를 저장할 수 있다.)
 ImageName = "1_front.png" # 이미지 파일 이름
 srcImage = imread(ImageName) # 이미지 파일 이름을 이용해 이미지를 가져온다. (같은 디렉토리 내에 위치 해있어야 함.)
+
 Dst = ctb.get_BinaryImage(srcImage)
 print srcImage.dtype, srcImage.shape, srcImage.size # 3차원 배열로 나옴
 print Dst.dtype, Dst.shape, Dst.size
@@ -22,9 +23,26 @@ height = 170.00 * 10 # 170.00 * 10 mm
 ratio = 0.0 # 픽셀 비율
 #ratio = GetRatio(Dst, height, ratio)
 
+# 여러가지 몸의 point들이 들어있는 class를 받아온다.
+
+#결과 값을 출력
+pp=bp.GetBodyPosition(srcImage)
+
+#손의 대한 내용을 출력한다.
+print("|---Print Hand--------|")
+print(pp.x)
+print(pp.y)
+# print(pp.left_hand)
+
+img = cv.circle(srcImage,(pp.x,pp.y),10,(0,0,255),-1)
+
+# print(pp.my_hand.my_right_hand)
+
+
+
 # 이미지 보이기
 cv.imshow('BodyFeature-Origin', srcImage) # 원본 이미지
-cv.imshow('BodyFeature', Dst) # 후처리 이미지
+cv.imshow('BodyFeature', img) # 후처리 이미지
 cv.waitKey()
 
 
