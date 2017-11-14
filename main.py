fb1aa0a8797a49daf9e5e10932ee711b3466c7eb
Yeonwoo <yeonwookang93@gmail.com>
2017-11-14 19:33:28 +0900

    배경처리 없이 정면 키, 양손 끝 점 구하기

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
 # coding=utf-8
+# Main class
 
-from scipy.misc import imread, imsave, imresize
-import cv2 as cv
+import cv2
+import bodycontour as bc # 윤곽선을 구하는 기능
+import bodyposition as bp # 신체의 특정점들을 가져오는 기능
 import numpy as np
 import copy
 
-import converttobinary as ctb
-import bodyposition as bp
-import hand as h
+# 사용자로부터 이미지를 받아옴
+# 빠른 테스트를 위해 이미지 경로 지정
+# 이미지 원본은 srcImage에 저장함
+imgPath = "body.jpg"
+srcImage = cv2.imread(imgPath,1)
 
-# 소스 이미지 가져오기
-# cv::Mat형 자료 -> numppy로 변환 가능 (numpy 배열로 python에서 이미지를 저장할 수 있다.)
-ImageName = "1_front.png" # 이미지 파일 이름, 현재 이미지를 1_front.png 외에 다른 것으로 바꾸었을 때 실행되지 않는 오류가 있음 - 원인 이미지 크기 (641 * 840)
-srcImage = imread(ImageName) # 이미지 파일 이름을 이용해 이미지를 가져온다. (같은 디렉토리 내에 위치 해있어야 함.)
+# 사용자로부터 받아온 이미지를 처리하여
+# 윤곽선을 구해온 이미지를 저장 (윤곽선을 여기서는 RGB(0, 255, 0) 형광 초록으로 하여 눈에 잘 띄게 하겠음)
+pcImage = bc.getProcessedImage(srcImage)
 
-Dst = ctb.get_BinaryImage(srcImage)
+# 윤곽선이 구해진 이미지를 사용해 특정점 구하기를 시작
 
-print srcImage.dtype, srcImage.shape, srcImage.size # 3차원 배열로 나옴
-#print Dst.dtype, Dst.shape, Dst.size
+# 손 찾아 점 찍기
+hand_point = bp.getHandPoints(pcImage) # 손의 좌표를 가져옴
+cv2.circle(pcImage, (hand_point.left_hand.x, hand_point.left_hand.y), 3, (0, 0, 255), -1) #왼손 찍기
+cv2.circle(pcImage, (hand_point.right_hand.x, hand_point.right_hand.y), 3, (0, 0, 255), -1) #오른손 찍기
 
-# 실제 키가 170 인 것으로 가정
-height = 170.00 * 10 # 170.00 * 10 mm
-ratio = 0.0 # 픽셀 비율
-#ratio = GetRatio(Dst, height, ratio)
+# 정면 키 찾아 점 찍기
+frontheight_point = bp.getFrontHeightPoints(pcImage) # 정면 키 좌표를 가져옴
+cv2.circle(pcImage, (frontheight_point.front_head.x, frontheight_point.front_head.y), 3, (0, 0, 255), -1) #머리점 찍기
+cv2.circle(pcImage, (frontheight_point.front_head.x, frontheight_point.front_foot.y), 3, (0, 0, 255), -1) #발끝점 찍기 - 발끝의 x는 머리의 x로 둠
 
-# 여러가지 몸의 point들이 들어있는 class를 받아온다.
 
-#결과 값을 출력
-pp=bp.GetBodyPosition(srcImage)
-pp2=bp.GetBodyPosition2(srcImage)
+#머리, 어깨, 허리, 손, 무릎, 발목, 발 선 긋기
+cv2.line(pcImage, (hand_point.left_hand.x, hand_point.left_hand.y), (hand_point.right_hand.x, hand_point.right_hand.y), (255, 0, 0), 1) #손
+cv2.line(pcImage, (frontheight_point.front_head.x, frontheight_point.front_head.y), (frontheight_point.front_head.x, frontheight_point.front_foot.y), (255, 0, 0), 1) #키 - 발끝의 x는 머리의 x로 둠
 
-#손의 대한 내용을 출력한다.
-print("|---Print Hand--------|")
+#임의 선
+cv2.line(pcImage, (frontheight_point.front_head.x, frontheight_point.front_head.y), (hand_point.left_hand.x, hand_point.left_hand.y), (255, 200, 0), 1) #머리-왼손
+cv2.line(pcImage, (frontheight_point.front_head.x, frontheight_point.front_head.y), (hand_point.right_hand.x, hand_point.right_hand.y), (255, 200, 0), 1) #머리-오른손
+cv2.line(pcImage, (frontheight_point.front_head.x, frontheight_point.front_foot.y), (hand_point.left_hand.x, hand_point.left_hand.y), (255, 200, 0), 1) #왼손-발
+cv2.line(pcImage, (frontheight_point.front_head.x, frontheight_point.front_foot.y), (hand_point.right_hand.x, hand_point.right_hand.y), (255, 200, 0), 1) #오른손-발
 
-    
-# 화면 가로선 그리기 - 후처리 이미지
-# 손
-th = cv.line(Dst, (0, pp.left_hand.y), (1000, pp.rignt_hand.y), (0, 0, 255), 1)  # 화면 크기의 x 최대 값으로 오른쪽 점 x좌표 값을 바꿔주어야 함
-# 머리선
-th2 = cv.line(Dst, (0, pp2.head.y), (1000, pp2.head.y), (0, 0, 255), 1)
-# 발선
-th3 = cv.line(Dst, (0, pp2.foot.y), (1000, pp2.foot.y), (0, 0, 255), 1)
 
-# 손 점 찍기
-img = cv.line(Dst,(pp.left_hand.x,pp.left_hand.y),(pp.left_hand.x,pp.left_hand.y),(255,0,0),10)
-img2 = cv.line(Dst,(pp.rignt_hand.x,pp.rignt_hand.y),(pp.rignt_hand.x,pp.rignt_hand.y),(255,0,0),10)
+# bodyPosition의 getPixelPerCm()를 이용하여 픽셀 수를 구해봄
+# 입력받은 키 / 머리에서 발까지 길이 = cm당 픽셀 수
 
-# 머리점
-img3 = cv.line(Dst,(pp2.head.x,pp2.head.y),(pp2.head.x,pp2.head.y ),(255,0,0),5)
-# 발끝점
-img4 = cv.line(Dst,(pp2.foot.x,pp2.foot.y),(pp2.foot.x,pp2.foot.y ),(255,0,0),5)
-
-# 후처리 이미지에 결과 값을 출력 (확인용)
-pp=bp.GetBodyPosition(Dst)
-# 이미지 보이기
-cv.imshow('BodyFeature-Origin', srcImage) # 원본 이미지
-cv.imshow('BodyFeature', Dst) # 후처리 이미지
-cv.waitKey()
-
+#원본 이미지 띄우기
+#cv2.imshow("OriginalImage", srcImage")
+# 처리된 이미지 띄우기
+cv2.imshow("ProcessedImage",pcImage)
+cv2.waitKey(0)
